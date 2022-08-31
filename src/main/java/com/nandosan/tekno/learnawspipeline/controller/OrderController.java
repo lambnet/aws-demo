593fa12407c5c40d1ccbc4f10d2cda50acb103d8
dwikikurniawn <Dwiki_K782@mitrais.com>
2022-08-31 16:53:43 +0700

    Revert "edit controller path"
    This reverts commit 21dcad3d11d8b4a10628c15afa84084cc1440f5f.

--- src/main/java/com/nandosan/tekno/learnawspipeline/controller/OrderController.java
+++ src/main/java/com/nandosan/tekno/learnawspipeline/controller/OrderController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class OrderController {
 
-    @GetMapping("/hello2")
+    @GetMapping("/hello")
     public String hello(){
-        return "Hello Dwiki";
+        return "Hello Fernando";
     }
 }
