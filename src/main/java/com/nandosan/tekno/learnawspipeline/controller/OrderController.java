0646110be2df4c89185d7e12d02b6464861ed4a7
Alvian Nur Ramadhani <alviannur.ramadhani@mitrais.com>
2022-11-02 12:54:04 +0700

    change request mapping

--- src/main/java/com/nandosan/tekno/learnawspipeline/controller/OrderController.java
+++ src/main/java/com/nandosan/tekno/learnawspipeline/controller/OrderController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/demo-alvian")
 public class OrderController {
 
-    @GetMapping("/hello2")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello Dwiki";
     }
